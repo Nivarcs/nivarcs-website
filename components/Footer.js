38b2ba360883983c1f880605c0e0b1de17d68975
Nivarcs <ai.tech.nivarcs@gmail.com>
2025-02-22 04:29:03 +0530

    Updated homepage with new content

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+// components/Footer.js
+export default function Footer() {
+    return (
+      <footer>
+        <p>Follow us on:</p>
+        <ul>
+          <li><a href="https://facebook.com">Facebook</a></li>
+          <li><a href="https://twitter.com">Twitter</a></li>
+          <li><a href="https://linkedin.com">LinkedIn</a></li>
+        </ul>
+        <p>Contact: info@nivarcs.com</p>
+      </footer>
+    );
+  }
+  
---
+++ components/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import Hero from './Hero';
+import ComingSoon from './ComingSoon';
+import Footer from './Footer';
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <ComingSoon />
+      <Footer />
+    </div>
+  );
+}
